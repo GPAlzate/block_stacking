c243521dd7fb28af1a3a9b57969a1b13757343b4
GPAlzate <ggalzate98@gmail.com>
2019-10-29 13:27:05 -0700

    hee

---
+++ BlockStacking.java
@@ -0,0 +1,14 @@
+public BlockStacking {
+
+    private int x, y, z;
+
+    public BlockStacking(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ block_stacking.py
@@ -0,0 +1,1 @@
+# nothing needed cause python is noob shit
