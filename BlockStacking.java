be298d58ec938b53b9c22bcf452982a96760836d
Janice Lee <janicelee@pop-os.localdomain>
2019-10-29 20:05:11 -0700

    succ my block

--- BlockStacking.java
+++ BlockStacking.java
@@ -1,11 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public BlockStacking {
 
-    private int x, y, z;
+    int n;
+    ArrayList<ArrayList<Integer>> blocks;
 
-    public BlockStacking(int x, int y, int z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    public BlockStacking(File input) {
+        blocks = new ArrayList<ArrayList<Integer>>();
+    }
+
+    private static ArrayList<ArrayList<Integer>> generate(File input) {
+        Scanner sc = new Scanner(input);
+        sc.useDelimiter("\n");
+        n = sc.next();
+        
+        ArrayList<Integer> block;
+        String[] line;
+
+        while (sc.hasNext()) {
+            block = new ArrayList<Integer>(n);
+            line = sc.next().split("\\s");
+
+            for(String s : line)
+                block.add(Integer.parseInt(s));
+
+            if(blocks.get(0) > blocks.get(1)){
+                int temp = blocks.get(0);
+                blocks.add(0, blocks.get(1));
+                blocks.add(1, temp);
+            }
+            
+            blocks.add(block);
+        }
+
     }
 
     public static void main(String[] args) {
