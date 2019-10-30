2a66e8522592e4fd5e5d6b85c734cd387f31fb36
jlad2017 <janicelee@pop-os.localdomain>
2019-10-29 20:56:59 -0700

    joicy

--- BlockStacking.java
+++ BlockStacking.java
@@ -1,43 +1,89 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Comparator;
 
-public BlockStacking {
+public class BlockStacking {
 
     int n;
     ArrayList<ArrayList<Integer>> blocks;
+    HashMap<ArrayList<Integer>, Integer> heights;
 
     public BlockStacking(File input) {
         blocks = new ArrayList<ArrayList<Integer>>();
+        heights = new HashMap<ArrayList<Integer>, Integer>();
+        generate(input);
     }
 
-    private static ArrayList<ArrayList<Integer>> generate(File input) {
-        Scanner sc = new Scanner(input);
-        sc.useDelimiter("\n");
-        n = sc.next();
-        
-        ArrayList<Integer> block;
-        String[] line;
+    private void generate(File input) {
 
-        while (sc.hasNext()) {
-            block = new ArrayList<Integer>(n);
-            line = sc.next().split("\\s");
+        try {
+            Scanner sc = new Scanner(input);
+            sc.useDelimiter("\n");
+            n = Integer.parseInt(sc.next());
 
-            for(String s : line)
-                block.add(Integer.parseInt(s));
+            ArrayList<Integer> block;
+            String[] line;
 
-            if(blocks.get(0) > blocks.get(1)){
-                int temp = blocks.get(0);
-                blocks.add(0, blocks.get(1));
-                blocks.add(1, temp);
+            while (sc.hasNext()) {
+                block = new ArrayList<Integer>(n);
+                line = sc.next().split("\\s");
+
+                for(String s : line)
+                    block.add(Integer.parseInt(s));
+
+                Collections.sort(block);
+                
+                heights.put(new ArrayList<> (block), 0);
+
+                block.add(1, block.remove(2));
+                heights.put(new ArrayList<> (block), 0);
+
+                block.add(0, block.remove(2));
+                block.add(1, block.remove(2));
+                heights.put(new ArrayList<> (block), 0);
+
             }
-            
-            blocks.add(block);
+
+            blocks = new ArrayList<ArrayList<Integer>>(heights.keySet());
+            Collections.sort(blocks, new Comparator<ArrayList<Integer>>(){
+
+                    public int compare(ArrayList<Integer> a, ArrayList<Integer> b){
+                        return a.get(0) - b.get(0);
+                    }
+
+                });
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void stackBlocks(){
+
+        // base case, 1 block in stack has max height of itself
+        heights.put(blocks.get(0), blocks.get(0).get(2));
+
+        List<Integer> current
+
+        for(int i = 1; i < blocks.size(); i++){
+            for(int j = 0; j < i; j++){
+
+                
+
+            }
         }
 
     }
 
     public static void main(String[] args) {
+        File input = new File(args[0]);
+        BlockStacking test = new BlockStacking(input);
+        System.out.println(test.blocks.toString());
     }
 
 }
